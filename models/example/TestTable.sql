cbb9c8142bd91ec70fccf894d35fd90ae6912743
Ali Hasan <Ali.Hasan@bentley.com>
2020-01-24 12:30:55 +0500

    Adding models

---
+++ models/example/TestTable.sql
@@ -0,0 +1,13 @@
+{{
+    config(materialized='persistent_table'
+        ,retain_previous_version_flg=false
+        ,migrate_data_over_flg=false
+    )
+}}
+
+CREATE OR REPLACE TABLE "{{ database }}"."{{ schema }}"."TESTTABLE" (
+ID varchar2 not null,
+FIRST_NAME text,
+LAST_NAME text,
+PRIMARY KEY (ID)
+)
