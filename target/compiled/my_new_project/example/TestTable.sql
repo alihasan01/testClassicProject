83728fef60aead3d48d861ebbd3cfa516b97555c
Ali Hasan <Ali.Hasan@bentley.com>
2020-01-27 18:44:31 +0500

    adding target folder

---
+++ target/compiled/my_new_project/example/TestTable.sql
@@ -0,0 +1,8 @@
+
+
+CREATE OR REPLACE TABLE "DEMO_DB"."PUBLIC"."TESTTABLE" (
+ID varchar2 not null,
+FIRST_NAME text,
+LAST_NAME text,
+PRIMARY KEY (ID)
+)
---
+++ target/run/my_new_project/example/TestTable.sql
@@ -0,0 +1,13 @@
+
+            
+            
+
+    
+
+CREATE OR REPLACE TABLE "DEMO_DB"."PUBLIC"."TESTTABLE_DBT_TMP" (
+ID varchar2 not null,
+FIRST_NAME text,
+LAST_NAME text,
+PRIMARY KEY (ID)
+)
+    ;
