ed5dfc08fdc2ff78a113fe07f2cc01469e3c8ac8
Ali Hasan <Ali.Hasan@bentley.com>
2020-01-28 11:30:19 +0500

    adding  column

--- models/example/TESTTABLE.sql
+++ models/example/TESTTABLE.sql
@@ -9,7 +9,6 @@ CREATE OR REPLACE TABLE "{{ database }}"."{{ schema }}"."TESTTABLE" (
 ID varchar2 not null,
 FIRST_NAME text,
 LAST_NAME text,
-CONTACT varchar2,
 STREET_NO number,
 PRIMARY KEY (ID)
 )
